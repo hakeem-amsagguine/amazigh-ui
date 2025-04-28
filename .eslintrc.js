e205853dfb9e94d780e94ea7ee3cbcc659ca840b
Hakeem A. <hakimaezr@gmail.com>
2025-04-28 19:09:58 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-hooks"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  ignorePatterns: ["dist", "node_modules", "**/*.css", "**/*.scss"],
+  env: {
+    browser: true,
+    node: true,
+  },
+};
